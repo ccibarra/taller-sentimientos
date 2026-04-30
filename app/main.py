36bdf14257deaf66a7c78b197a1839d4003762ef
ccibarra <61798354+ccibarra@users.noreply.github.com>
2026-04-29 23:31:04 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,7 +27,6 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "super-secret-key")
 
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
-ALLOWED_DOMAIN = "gmail.com"  # Cambia esto por tu dominio de Workspace si es necesario
 
 # ==============================
 # VALIDAR TOKEN GOOGLE
@@ -42,8 +41,9 @@ def verify_google_token(token):
         )
 
         # Validar dominio Workspace
-        if idinfo.get("hd") != ALLOWED_DOMAIN:
-            return None
+    email = idinfo.get("email")
+    if not email.endswith("@gmail.com"):
+    return None
 
         return idinfo
 
